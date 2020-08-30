3197fd8d3f4592cd23496ad7eb442a9aa7e1a58a
Marek Musiał <mmusialmarek@gmail.com>
2020-08-30 10:01:18 +0200

    INITIAL COMMIT

---
+++ src/main/java/pl/jgora/aeroklub/airflightslist/AirFlightsListApplication.java
@@ -0,0 +1,13 @@
+package pl.jgora.aeroklub.airflightslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirFlightsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirFlightsListApplication.class, args);
+	}
+
+}
