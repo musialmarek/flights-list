a84118aa670fcd2baa05d3d2e515e0b6842ef130
Marek Musiał <mmusialmarek@gmail.com>
2020-08-30 13:59:33 +0200

    created PilotService

---
+++ src/main/java/pl/jgora/aeroklub/airflightslist/pilot/PilotService.java
@@ -0,0 +1,17 @@
+package pl.jgora.aeroklub.airflightslist.pilot;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.jgora.aeroklub.airflightslist.model.Pilot;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PilotService {
+    private final PilotRepository pilotRepository;
+
+    List<Pilot> findAll() {
+        return null;
+    }
+}
