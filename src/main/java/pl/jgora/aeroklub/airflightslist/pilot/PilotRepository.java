aed8fbf9b17f09e23676f0f8460a44eb15510b4c
Marek Musiał <mmusialmarek@gmail.com>
2020-08-30 14:06:28 +0200

    added method to get all pilots with ordering by name

--- src/main/java/pl/jgora/aeroklub/airflightslist/pilot/PilotRepository.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/pilot/PilotRepository.java
@@ -3,5 +3,8 @@ package pl.jgora.aeroklub.airflightslist.pilot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.jgora.aeroklub.airflightslist.model.Pilot;
 
+import java.util.List;
+
 public interface PilotRepository extends JpaRepository<Pilot,Long> {
+    List<Pilot> findPilotsByOrderByName();
 }
