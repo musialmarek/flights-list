a79382f11e3ba1ab24ec22a07419a3821b37b02a
Marek Musiał <mmusialmarek@gmail.com>
2020-09-01 16:16:35 +0200

    added method to update(edit) Pilot

--- src/main/java/pl/jgora/aeroklub/airflightslist/pilot/PilotService.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/pilot/PilotService.java
@@ -16,11 +16,32 @@ public class PilotService {
     }
 
     Pilot findById(Long id) {
-        return pilotRepository.getOne(id);
+        return pilotRepository.findFirstById(id);
     }
 
-    void update(Pilot pilot){
+    void activationUpdate(Pilot pilot) {
         pilotRepository.save(pilot);
     }
 
+    void update(Pilot pilot) {
+        if (pilot != null && pilot.getId() != null) {
+            Pilot toEdit = findById(pilot.getId());
+            toEdit.setFiA(pilot.getFiA());
+            toEdit.setFiS(pilot.getFiS());
+            toEdit.setSEPL(pilot.getSEPL());
+            toEdit.setActive(pilot.getActive());
+            toEdit.setEngineInstructor(pilot.getEngineInstructor());
+            toEdit.setEnginePilot(pilot.getEnginePilot());
+            toEdit.setEnginePractise(pilot.getEnginePractise());
+            toEdit.setGliderInstructor(pilot.getGliderInstructor());
+            toEdit.setGliderPilot(pilot.getGliderPilot());
+            toEdit.setGliderPractise(pilot.getGliderPractise());
+            toEdit.setLicence(pilot.getLicence());
+            toEdit.setMedicine(pilot.getMedicine());
+            toEdit.setName(pilot.getName());
+            toEdit.setTheory(pilot.getTheory());
+            toEdit.setTow(pilot.getTow());
+            pilotRepository.save(toEdit);
+        }
+    }
 }
