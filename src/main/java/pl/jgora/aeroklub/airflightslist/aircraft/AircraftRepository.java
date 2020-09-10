2baa720fe57ba663097376def8740c1f57ec3aee
Marek Musiał <mmusialmarek@gmail.com>
2020-09-10 22:02:53 +0200

    created AircraftRepository

---
+++ src/main/java/pl/jgora/aeroklub/airflightslist/aircraft/AircraftRepository.java
@@ -0,0 +1,12 @@
+package pl.jgora.aeroklub.airflightslist.aircraft;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.jgora.aeroklub.airflightslist.model.Aircraft;
+
+import java.util.List;
+
+public interface AircraftRepository extends JpaRepository<Aircraft,Long> {
+    List<Aircraft> findAircraftsByOrderByType();
+
+    Aircraft findFirstById(Long id);
+}
