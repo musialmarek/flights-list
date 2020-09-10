50cfc2043149b0024779472f2a31b76829f2ecce
Marek Musiał <mmusialmarek@gmail.com>
2020-09-10 22:17:28 +0200

    created AircraftService

---
+++ src/main/java/pl/jgora/aeroklub/airflightslist/aircraft/AircraftService.java
@@ -0,0 +1,49 @@
+package pl.jgora.aeroklub.airflightslist.aircraft;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import pl.jgora.aeroklub.airflightslist.model.Aircraft;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AircraftService {
+    private final AircraftRepository aircraftRepository;
+
+    List<Aircraft> findAll() {
+        return aircraftRepository.findAircraftsByOrderByType();
+    }
+
+    Aircraft findById(Long id) {
+        return aircraftRepository.findFirstById(id);
+    }
+
+    void activationUpdate(Aircraft aircraft) {
+        aircraftRepository.save(aircraft);
+    }
+
+    void update(Aircraft aircraft) {
+        if (aircraft != null && aircraft.getId() != null) {
+            Aircraft toEdit = findById(aircraft.getId());
+            toEdit.setActive(aircraft.getActive());
+            toEdit.setEngine(aircraft.getEngine());
+            toEdit.setType(aircraft.getType());
+            toEdit.setRegistrationNumber(aircraft.getRegistrationNumber());
+            toEdit.setFlyingTime(aircraft.getFlyingTime());
+            toEdit.setArc(aircraft.getArc());
+            toEdit.setInsuring(aircraft.getInsuring());
+            toEdit.setNextWorkDate(aircraft.getNextWorkDate());
+            toEdit.setNextWorkDateDescription(aircraft.getNextWorkDateDescription());
+            toEdit.setNextWorkTime(aircraft.getNextWorkTime());
+            toEdit.setNextWorkTimeDescription(aircraft.getNextWorkTimeDescription());
+            toEdit.setFlyingTimeHours(aircraft.getFlyingTimeHours());
+            toEdit.setFlyingTimeMinutes(aircraft.getFlyingTimeMinutes());
+            toEdit.setWorkTimeHours(aircraft.getWorkTimeHours());
+            toEdit.setWorkTimeMinutes(aircraft.getWorkTimeMinutes());
+            aircraftRepository.save(toEdit);
+        }
+    }
+}
