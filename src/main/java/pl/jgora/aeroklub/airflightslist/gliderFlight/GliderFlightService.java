42a03fb08fc3f467b39be65dcac90ada4d4e1d3b
Marek Musiał <mmusialmarek@gmail.com>
2020-09-12 17:00:22 +0200

    created GliderFlightService empty

---
+++ src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightService.java
@@ -0,0 +1,12 @@
+package pl.jgora.aeroklub.airflightslist.gliderFlight;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class GliderFlightService {
+    private final GliderFlightRepository gliderFlightRepository;
+}
