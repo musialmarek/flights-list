fe80c8742d63b8527c61046313bece1132462c21
Marek Musiał <mmusialmarek@gmail.com>
2020-09-12 16:59:09 +0200

    added method to get all flying dates

--- src/main/java/pl/jgora/aeroklub/airflightslist/engineFlight/EngineFlightRepository.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/engineFlight/EngineFlightRepository.java
@@ -1,8 +1,13 @@
 package pl.jgora.aeroklub.airflightslist.engineFlight;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.jgora.aeroklub.airflightslist.model.EngineFlight;
 
-public interface EngineFlightRepository extends JpaRepository<EngineFlight, Long> {
+import java.time.LocalDate;
+import java.util.Set;
 
+public interface EngineFlightRepository extends JpaRepository<EngineFlight, Long> {
+    @Query("SELECT e.date FROM EngineFlight e WHERE e.tow =false or e.tow is null and :start < e.date and e.date < :finish")
+    Set<LocalDate> getFlyingEngineDays(LocalDate start, LocalDate finish);
 }
