cec7000d706bf868242da2e52b592e28b80c83ce
Marek Musiał <mmusialmarek@gmail.com>
2020-09-16 17:46:22 +0200

    created AbstractFlightService for duplicated code

---
+++ src/main/java/pl/jgora/aeroklub/airflightslist/AbstractFlight/AbstractFlightService.java
@@ -0,0 +1,38 @@
+package pl.jgora.aeroklub.airflightslist.AbstractFlight;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import pl.jgora.aeroklub.airflightslist.model.AbstractFlight;
+import pl.jgora.aeroklub.airflightslist.model.Pilot;
+
+@Service
+@Slf4j
+
+
+public class AbstractFlightService {
+    public static void updateFlight(AbstractFlight flight, AbstractFlight toEdit) {
+        toEdit.setActive(flight.getActive());
+        toEdit.setAircraftRegistrationNumber(flight.getAircraftRegistrationNumber());
+        toEdit.setCopilotName(flight.getCopilotName());
+        toEdit.setDate(flight.getDate());
+        toEdit.setFlightTime(flight.getFlightTime());
+        toEdit.setInstructor(flight.getInstructor());
+        toEdit.setPicName(flight.getPicName());
+        toEdit.setStart(flight.getStart());
+        toEdit.setTask(flight.getTask());
+        toEdit.setTouchdown(flight.getTouchdown());
+        toEdit.setAircraft(flight.getAircraft());
+        toEdit.setCopilot(flight.getCopilot());
+        toEdit.setPic(flight.getPic());
+    }
+
+    public static void replacePilots(AbstractFlight flight) {
+        if (flight.getInstructor()) {
+            log.debug("\n REPLACING PILOTS - FLIGHT IS INSTRUCTOR FLIGHT ");
+            Pilot pic = flight.getCopilot();
+            Pilot copilot = flight.getPic();
+            flight.setPic(pic);
+            flight.setCopilot(copilot);
+        }
+    }
+}
