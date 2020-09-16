43b5c158eb77a7f771a5048b68296a41b7950f54
Marek Musiał <mmusialmarek@gmail.com>
2020-09-16 14:53:41 +0200

    added method to save flight

--- src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightService.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightService.java
@@ -3,7 +3,10 @@ package pl.jgora.aeroklub.airflightslist.gliderFlight;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pl.jgora.aeroklub.airflightslist.engineFlight.EngineFlightService;
+import pl.jgora.aeroklub.airflightslist.model.EngineFlight;
 import pl.jgora.aeroklub.airflightslist.model.GliderFlight;
+import pl.jgora.aeroklub.airflightslist.model.Pilot;
 
 import java.time.LocalDate;
 import java.util.Set;
@@ -13,6 +16,7 @@ import java.util.Set;
 @Slf4j
 public class GliderFlightService {
     private final GliderFlightRepository gliderFlightRepository;
+    private final EngineFlightService engineFlightService;
 
     public Set<LocalDate> getAllFlyingDays(int year) {
         LocalDate start = LocalDate.of(year, 1, 1);
@@ -22,5 +26,20 @@ public class GliderFlightService {
 
     public Set<GliderFlight> getByDate(LocalDate date) {
         return gliderFlightRepository.getDistinctByDateOrderByStart(date);
+    }
+
+    public GliderFlight save(GliderFlight flight) {
+        if (flight.getInstructor()) {
+            log.debug("\n SWAPPING PLACES - FLIGHT IS INSTRUCTOR FLIGHT ");
+            Pilot pic = flight.getCopilot();
+            Pilot copilot = flight.getPic();
+            flight.setPic(pic);
+            flight.setCopilot(copilot);
+        }
+        EngineFlight engineFlight = flight.getEngineFlight();
+        log.debug("\n SAVING TOW FLIGHT {}", engineFlight);
+        engineFlightService.save(engineFlight);
+        log.debug("\n SAVING GLIDER FLIGHT {}", flight);
+        return gliderFlightRepository.save(flight);
     }
 }
