94e3a8a7b7bdd1c1fe380b3a9e937e199d8cf031
Marek Musiał <mmusialmarek@gmail.com>
2020-09-16 17:47:43 +0200

    added method to update GliderFlight and extract replacing Pilots to external class

--- src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightService.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightService.java
@@ -3,12 +3,15 @@ package pl.jgora.aeroklub.airflightslist.gliderFlight;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pl.jgora.aeroklub.airflightslist.AbstractFlight.AbstractFlightService;
 import pl.jgora.aeroklub.airflightslist.engineFlight.EngineFlightService;
 import pl.jgora.aeroklub.airflightslist.model.EngineFlight;
 import pl.jgora.aeroklub.airflightslist.model.GliderFlight;
-import pl.jgora.aeroklub.airflightslist.model.Pilot;
+import pl.jgora.aeroklub.airflightslist.model.StartMethod;
 
 import java.time.LocalDate;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
 
 @Service
@@ -29,22 +32,33 @@ public class GliderFlightService {
     }
 
     public GliderFlight save(GliderFlight flight) {
-        if (flight.getInstructor()) {
-            log.debug("\n SWAPPING PLACES - FLIGHT IS INSTRUCTOR FLIGHT ");
-            Pilot pic = flight.getCopilot();
-            Pilot copilot = flight.getPic();
-            flight.setPic(pic);
-            flight.setCopilot(copilot);
-        }
+        AbstractFlightService.replacePilots(flight);
         EngineFlight engineFlight = flight.getEngineFlight();
         log.debug("\n SAVING TOW FLIGHT {}", engineFlight);
         engineFlightService.save(engineFlight);
         log.debug("\n SAVING GLIDER FLIGHT {}", flight);
         return gliderFlightRepository.save(flight);
     }
+
     public GliderFlight getById(Long id) {
         return gliderFlightRepository.findFirstById(id);
     }
 
+    public void update(GliderFlight flight) {
+        log.debug("\nCHECKING GLIDER FLIGHT {}", flight);
+        if (flight != null && flight.getId() != null) {
+            log.debug("\nGETTING GLIDER-FLIGHT FROM DATABASE");
+            GliderFlight toEdit = gliderFlightRepository.findFirstById(flight.getId());
+            log.debug("\n SETTING ALL FIELDS IN GLIDER-FLIGHT TO EDIT \n OLD DATA {} \n NEW DATA{}", toEdit, flight);
+            toEdit.setStartMethod(flight.getStartMethod());
+            toEdit.setEngineFlight(null);
+            if (flight.getStartMethod().equals(StartMethod.ATTO)) {
+                toEdit.setEngineFlight(engineFlightService.update(flight.getEngineFlight()));
+            }
+            AbstractFlightService.updateFlight(flight, toEdit);
+            log.debug("SAVING GLIDER-FLIGHT WITH NEW DATA");
+            gliderFlightRepository.save(toEdit);
+        }
+    }
 
 }
