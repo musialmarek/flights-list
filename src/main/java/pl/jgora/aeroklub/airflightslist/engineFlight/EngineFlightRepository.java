87cb4e89123ca9c198dc09f37a1b9fdcc805975a
Marek Musiał <mmusialmarek@gmail.com>
2020-09-17 19:59:30 +0200

    fixed wrong query

--- src/main/java/pl/jgora/aeroklub/airflightslist/engineFlight/EngineFlightRepository.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/engineFlight/EngineFlightRepository.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.Set;
 
 public interface EngineFlightRepository extends JpaRepository<EngineFlight, Long> {
-    @Query("SELECT e.date FROM EngineFlight e WHERE e.tow =false or e.tow is null and :start < e.date and e.date < :finish")
+    @Query("SELECT e.date FROM EngineFlight e WHERE :start < e.date and e.date < :finish and e.tow = false or e.tow is null")
     Set<LocalDate> getFlyingEngineDays(LocalDate start, LocalDate finish);
 
     Set<EngineFlight> getDistinctByDateOrderByStart(LocalDate date);
