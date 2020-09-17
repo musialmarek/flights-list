8a71455cc739b2a8a6d8a666c14141046f94c90f
Marek Musiał <mmusialmarek@gmail.com>
2020-09-17 09:59:34 +0200

    added actions to activate/deactivate all glider-flights and tow-flights in date

--- src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightController.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.jgora.aeroklub.airflightslist.AbstractFlight.AbstractFlightControllerUtil;
@@ -45,5 +46,29 @@ public class GliderFlightController {
             return "flights/glider-flight-details";
         }
         return gliderDailyFlights(model, date);
+    }
+
+    @PostMapping("/activate")
+    public String activateList(@RequestParam("date") String date) {
+        log.debug("\nACTIVATING LIST FROM {}", date);
+        Set<GliderFlight> flights = gliderFlightService.getByDate(LocalDate.parse(date));
+        for (GliderFlight flight : flights) {
+            flight.setActive(true);
+            flight.getEngineFlight().setActive(true);
+            gliderFlightService.update(flight);
+        }
+        return "redirect:/admin/glider-flights";
+    }
+
+    @PostMapping("/deactivate")
+    public String deactivateList(@RequestParam("date") String date) {
+        log.debug("\nDEACTIVATING LIST FROM {}", date);
+        Set<GliderFlight> flights = gliderFlightService.getByDate(LocalDate.parse(date));
+        for (GliderFlight flight : flights) {
+            flight.setActive(false);
+            flight.getEngineFlight().setActive(true);
+            gliderFlightService.update(flight);
+        }
+        return "redirect:/admin/glider-flights";
     }
 }
