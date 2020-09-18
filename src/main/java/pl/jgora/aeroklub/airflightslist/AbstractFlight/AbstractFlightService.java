4a0e8b4a83aee710f55f12b1b2f501b357707bcf
Marek Musiał <mmusialmarek@gmail.com>
2020-09-18 16:02:40 +0200

    added method to build "where section" to query to filtering flights by a pilot

--- src/main/java/pl/jgora/aeroklub/airflightslist/AbstractFlight/AbstractFlightService.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/AbstractFlight/AbstractFlightService.java
@@ -3,7 +3,12 @@ package pl.jgora.aeroklub.airflightslist.AbstractFlight;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pl.jgora.aeroklub.airflightslist.model.AbstractFlight;
+import pl.jgora.aeroklub.airflightslist.model.Aircraft;
 import pl.jgora.aeroklub.airflightslist.model.Pilot;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -34,5 +39,78 @@ public class AbstractFlightService {
             flight.setPic(pic);
             flight.setCopilot(copilot);
         }
+    }
+
+    public static void getWhereSectionFilteringFlightsByPilot(Pilot pilot, Boolean active, String from, String to, String task, Boolean pic, Boolean instructor, Aircraft aircraft, String type, String registration, StringBuilder whereSectionBuilder, Map<String, Object> filters) {
+        if (active != null) {
+            log.debug("\nACTIVE: {}", active);
+            whereSectionBuilder.append(" f.active=").append(active).append(" AND");
+        }
+        if (from != null && !from.isEmpty()) {
+            log.debug("\nFROM: {}", from);
+            try {
+                LocalDate fromDate = LocalDate.parse(from);
+                whereSectionBuilder.append(" f.date >= :from AND");
+                filters.put("from", fromDate);
+
+            } catch (DateTimeParseException e) {
+                e.printStackTrace();
+                log.debug("\nWRONG DATA FORMAT IN \"FROM\" PARAMETER");
+            }
+        }
+        if (to != null && !to.isEmpty()) {
+            log.debug("\nTO: {}", to);
+            try {
+                LocalDate toDate = LocalDate.parse(to);
+                whereSectionBuilder.append(" f.date <= :to AND");
+                filters.put("to", toDate);
+
+            } catch (DateTimeParseException e) {
+                e.printStackTrace();
+                log.debug("\nWRONG DATA FORMAT IN \"TO\" PARAMETER");
+            }
+        }
+        if (task != null && !task.isEmpty()) {
+            log.debug("\nTASK: {}", task);
+            whereSectionBuilder.append(" f.task like concat('%',:task,'%') AND");
+            filters.put("task", task);
+        }
+        if (pic != null) {
+            log.debug("\nPIC: {}", pic);
+            if (pic == true) {
+                whereSectionBuilder.append(" (f.pic = :pilot OR f.picName like concat('%',:name,'%')) AND");
+            } else {
+                whereSectionBuilder.append(" (f.copilot = :pilot OR f.copilotName like concat('%',:name,'%')) AND");
+            }
+
+        } else {
+            log.debug("\nPIC IS NULL");
+            whereSectionBuilder.append(" (f.pic = :pilot OR f.picName = :name OR f.copilot = :pilot OR f.copilotName = :name) AND");
+        }
+        filters.put("pilot", pilot);
+        filters.put("name", pilot.getName());
+        if (instructor != null) {
+            log.debug("INSTRUCTOR: {}", instructor);
+            whereSectionBuilder.append(" f.instructor=").append(instructor).append(" AND");
+        }
+        if (aircraft != null) {
+            log.debug("\nAIRCRAFT : {}", aircraft);
+            whereSectionBuilder.append(" (f.aircraft = :aircraft OR (f.aircraft.type = :type AND f.aircraft.registrationNumber = :registration)) AND");
+            filters.put("aircraft", aircraft);
+            filters.put("type", aircraft.getType());
+            filters.put("registration", aircraft.getRegistrationNumber());
+        } else {
+            if (type != null && !type.isEmpty()) {
+                log.debug("\nTYPE : {}", type);
+                whereSectionBuilder.append(" f.aircraft.type like concat('%',:type,'%') AND");
+                filters.put("type", type);
+            }
+            if (registration != null && !registration.isEmpty()) {
+                log.debug("\nREGISTRATION {}", registration);
+                whereSectionBuilder.append(" f.aircraft.registrationNumber like concat('%',:registration,'%') AND");
+                filters.put("registration", registration);
+            }
+        }
+        whereSectionBuilder.append(" f.id IS NOT NULL ");
     }
 }
