384c286c278a98dc7e86650793ba7badf1eb543b
Marek Musiał <mmusialmarek@gmail.com>
2020-09-18 16:08:18 +0200

    added extends to flight's repositories - FilterFlightsRepository

--- src/main/java/pl/jgora/aeroklub/airflightslist/engineFlight/EngineFlightRepository.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/engineFlight/EngineFlightRepository.java
@@ -2,16 +2,21 @@ package pl.jgora.aeroklub.airflightslist.engineFlight;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import pl.jgora.aeroklub.airflightslist.AbstractFlight.FilterFlightsRepository;
 import pl.jgora.aeroklub.airflightslist.model.EngineFlight;
+import pl.jgora.aeroklub.airflightslist.model.Pilot;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
-public interface EngineFlightRepository extends JpaRepository<EngineFlight, Long> {
+public interface EngineFlightRepository extends JpaRepository<EngineFlight, Long>, FilterFlightsRepository {
     @Query("SELECT e.date FROM EngineFlight e WHERE :start < e.date and e.date < :finish and e.tow = false or e.tow is null")
     Set<LocalDate> getFlyingEngineDays(LocalDate start, LocalDate finish);
 
     Set<EngineFlight> getDistinctByDateOrderByStart(LocalDate date);
 
     EngineFlight findFirstById(Long id);
+
+    List<EngineFlight> findByPicOrCopilotOrPicNameLikeOrCopilotNameLike(Pilot pic,Pilot copilot,String picName, String copilotName);
 }
--- src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightRepository.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightRepository.java
@@ -2,16 +2,22 @@ package pl.jgora.aeroklub.airflightslist.gliderFlight;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import pl.jgora.aeroklub.airflightslist.AbstractFlight.FilterFlightsRepository;
 import pl.jgora.aeroklub.airflightslist.model.GliderFlight;
+import pl.jgora.aeroklub.airflightslist.model.Pilot;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 
-public interface GliderFlightRepository extends JpaRepository<GliderFlight, Long> {
+public interface GliderFlightRepository extends JpaRepository<GliderFlight, Long>, FilterFlightsRepository {
     @Query("SELECT e.date FROM GliderFlight e WHERE :start < e.date and e.date < :finish")
     Set<LocalDate> getFlyingGliderDays(LocalDate start, LocalDate finish);
 
     Set<GliderFlight> getDistinctByDateOrderByStart(LocalDate date);
 
     GliderFlight findFirstById(Long id);
+
+    List<GliderFlight> findByPicOrCopilotOrPicNameLikeOrCopilotNameLike(Pilot pic, Pilot copilot, String picName, String copilotName);
+
 }
