19164fd969f92357b7b0c196606e82d0919c82d5
Marek Musiał <mmusialmarek@gmail.com>
2020-09-22 00:31:51 +0200

    created entity User

---
+++ src/main/java/pl/jgora/aeroklub/airflightslist/model/User.java
@@ -0,0 +1,30 @@
+package pl.jgora.aeroklub.airflightslist.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@Setter
+@Getter
+@EqualsAndHashCode(of = {"id"})
+@ToString(exclude = {"pilot"})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String login;
+    private String password;
+    @Column(unique = true)
+    private String email;
+    @ManyToOne
+    private Role role;
+    @OneToOne
+    private Pilot pilot;
+    private Boolean active;
+}
