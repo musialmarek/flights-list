bd2bc7ed22a2b1125a32057311df2e3709714ccd
Marek Musiał <mmusialmarek@gmail.com>
2020-09-22 00:38:02 +0200

    optimised imports

--- src/main/java/pl/jgora/aeroklub/airflightslist/user/UserController.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/user/UserController.java
@@ -2,12 +2,9 @@ package pl.jgora.aeroklub.airflightslist.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import pl.jgora.aeroklub.airflightslist.model.User;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,8 +13,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("admin/users")
-    public String showUsers(Model model){
-        model.addAttribute("users",userService.findAll());
+    public String showUsers(Model model) {
+        model.addAttribute("users", userService.findAll());
         return "users/users";
     }
 
