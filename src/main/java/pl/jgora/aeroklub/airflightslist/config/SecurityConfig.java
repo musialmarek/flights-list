7cba302fc3423584eb143c9bdd9526f832529200
Marek Musiał <mmusialmarek@gmail.com>
2020-09-23 00:23:15 +0200

    changed homepage endpoint  to authenticated

--- src/main/java/pl/jgora/aeroklub/airflightslist/config/SecurityConfig.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/").authenticated()
                 .antMatchers("/admin", "/admin/**").hasAnyRole("ADMIN")
                 .antMatchers("/user", "/user/**").hasAnyRole("ADMIN", "USER")
                 .and().formLogin().permitAll().loginPage("/login")
