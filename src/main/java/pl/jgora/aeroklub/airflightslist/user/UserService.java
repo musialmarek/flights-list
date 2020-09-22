2b1829cd3a643ccc075136f95da60f2b1cec2355
Marek Musiał <mmusialmarek@gmail.com>
2020-09-23 01:49:34 +0200

    added method to get availablePilots

--- src/main/java/pl/jgora/aeroklub/airflightslist/user/UserService.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/user/UserService.java
@@ -1,11 +1,13 @@
 package pl.jgora.aeroklub.airflightslist.user;
 
+import pl.jgora.aeroklub.airflightslist.model.Pilot;
 import pl.jgora.aeroklub.airflightslist.model.User;
 
 import java.util.Set;
 
 public interface UserService {
     User findByUserName(String userName);
+
     void saveUser(User user);
 
     Set<User> findAll();
@@ -13,4 +15,8 @@ public interface UserService {
     User findById(Long id);
 
     void activationUpdate(User toDeactivate);
+
+    void updateUser(User user);
+
+    Set<Pilot> getAvailablePilots();
 }
--- src/main/java/pl/jgora/aeroklub/airflightslist/user/UserServiceImpl.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/user/UserServiceImpl.java
@@ -1,20 +1,28 @@
 package pl.jgora.aeroklub.airflightslist.user;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import pl.jgora.aeroklub.airflightslist.model.Pilot;
 import pl.jgora.aeroklub.airflightslist.model.Role;
 import pl.jgora.aeroklub.airflightslist.model.User;
+import pl.jgora.aeroklub.airflightslist.pilot.PilotService;
 import pl.jgora.aeroklub.airflightslist.role.RoleRepository;
 
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final PilotService pilotService;
 
 
     @Override
@@ -24,10 +32,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void saveUser(User user) {
+        log.debug("\n ENCODING PASSWORD");
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setActive(true);
-        Role userRole = roleRepository.findByName("ROLE_USER");
-        user.setRole(userRole);
+        if (user.getRole() == null) {
+            Role userRole = roleRepository.findByName("ROLE_USER");
+            user.setRole(userRole);
+        }
         userRepository.save(user);
     }
 
@@ -43,6 +54,31 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void activationUpdate(User user) {
-        userRepository.save(user);
+        User toEdit = userRepository.findFirstById(user.getId());
+        toEdit.setActive(user.getActive());
+        userRepository.save(toEdit);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        User toEdit = userRepository.findFirstById(user.getId());
+        log.debug("\n CHANGING USER'S DATA");
+        toEdit.setPassword(user.getPassword());
+        toEdit.setActive(user.getActive());
+        toEdit.setPilot(user.getPilot());
+        toEdit.setEmail(user.getEmail());
+        toEdit.setRole(user.getRole());
+        log.debug("\n SAVING EDITED USER");
+        saveUser(toEdit);
+    }
+
+    @Override
+    public Set<Pilot> getAvailablePilots() {
+        return pilotService
+                .findAll()
+                .stream()
+                .filter(pilot -> !userRepository.findAllUnavailablePilots().contains(pilot))
+                .sorted(Comparator.comparing(Pilot::getName))
+                .collect(Collectors.toCollection(LinkedHashSet::new));
     }
 }
