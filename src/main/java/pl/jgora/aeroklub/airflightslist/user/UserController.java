01e251ae29d3a899ce13ea83262a9ba5528e8376
Marek Musiał <mmusialmarek@gmail.com>
2020-09-23 01:47:00 +0200

    added action to show admin and user dashboard and user details for users

--- src/main/java/pl/jgora/aeroklub/airflightslist/user/UserController.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/user/UserController.java
@@ -2,9 +2,11 @@ package pl.jgora.aeroklub.airflightslist.user;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.jgora.aeroklub.airflightslist.model.User;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,10 +14,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("admin/users")
+    @GetMapping("/admin/users")
     public String showUsers(Model model) {
+        log.debug("\nADDING LIST ALL USERS TO MODEL");
         model.addAttribute("users", userService.findAll());
         return "users/users";
+    }
+
+    @GetMapping("/user")
+    public String userDashboard(Model model, @AuthenticationPrincipal CurrentUser currentUser) {
+        log.debug("\nADDING USER AND USER'S PILOT TO MODEL");
+        User user = userService.findById(currentUser.getUser().getId());
+        model.addAttribute("user", user);
+        model.addAttribute("pilot", user.getPilot());
+        return "users/user-dashboard";
+    }
+
+    @GetMapping("/admin")
+    public String adminDashboard(Model model, @AuthenticationPrincipal CurrentUser currentUser) {
+        log.debug("\nADDING USER (ADMIN)  TO MODEL");
+        User user = userService.findById(currentUser.getUser().getId());
+        model.addAttribute("user", user);
+        model.addAttribute("pilot", user.getPilot());
+        return "users/admin-dashboard";
+    }
+
+    @GetMapping("/user/details")
+    public String userDetails(Model model, @AuthenticationPrincipal CurrentUser currentUser) {
+        log.debug("\nADDING USER AND USER'S PILOT TO MODEL");
+        User user = userService.findById(currentUser.getUser().getId());
+        model.addAttribute("user", user);
+        model.addAttribute("pilot", user.getPilot());
+        return "users/user-details";
+
     }
 
 }
