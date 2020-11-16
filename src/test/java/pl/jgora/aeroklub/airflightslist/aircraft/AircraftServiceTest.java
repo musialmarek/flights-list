3be06008d10d2c53456a65154e632feb9ceadb4e
Marek Musiał <mmusialmarek@gmail.com>
2020-11-16 19:51:41 +0100

    added other test

--- src/test/java/pl/jgora/aeroklub/airflightslist/aircraft/AircraftServiceTest.java
+++ src/test/java/pl/jgora/aeroklub/airflightslist/aircraft/AircraftServiceTest.java
@@ -1,47 +1,140 @@
 package pl.jgora.aeroklub.airflightslist.aircraft;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import pl.jgora.aeroklub.airflightslist.model.Aircraft;
 
-import static org.mockito.Mockito.mock;
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
 
 @Slf4j
 class AircraftServiceTest {
     private static AircraftService testingObject;
     private static AircraftRepository aircraftRepository;
 
-    @BeforeAll
-    static void setUp() {
+    @BeforeEach
+    void setUp() {
         aircraftRepository = mock(AircraftRepository.class);
         testingObject = new AircraftService(aircraftRepository);
     }
 
     @Test
-    void findAll() {
+    void shouldFindingInDatabaseAllAircraftsOrderByType() {
+        testingObject.findAll();
+
+        verify(aircraftRepository, times(1)).findAircraftsByOrderByType();
     }
 
     @Test
-    void findById() {
+    void shouldFindingInDatabaseOneAircraftById() {
+        long aLong = anyLong();
+        testingObject.findById(aLong);
+
+        verify(aircraftRepository, times(1)).findFirstById(aLong);
     }
 
     @Test
-    void activationUpdate() {
+    void shouldSetActiveFalseAndSaveToDatabaseWhenActiveArgIsFalse() {
+        //Given
+        Aircraft activeAircraft = AircraftTestBase.builder().withActive(true).build();
+        //When
+        testingObject.activationUpdate(activeAircraft, false);
+        //Then
+        assertThat(activeAircraft.getActive()).isFalse();
+        verify(aircraftRepository, times(1)).save(activeAircraft);
     }
 
     @Test
-    void update() {
+    void shouldSetActiveTrueAndSaveToDatabaseWhenActiveArgIsTrue() {
+        //Given
+        Aircraft inactiveAircraft = AircraftTestBase.builder().withActive(false).build();
+        //When
+        testingObject.activationUpdate(inactiveAircraft, true);
+        //Then
+        assertThat(inactiveAircraft.getActive()).isTrue();
+        verify(aircraftRepository, times(1)).save(inactiveAircraft);
     }
 
     @Test
-    void filteredAircrafts() {
+    void shouldFindingAircraftByIdInDataBaseWhenUpdate() {
+        //Given
+        Aircraft aircraft = AircraftTestBase.builder().build();
+        //When
+        testingObject.update(aircraft);
+        //Then
+        verify(aircraftRepository, times(1)).findFirstById(aircraft.getId());
     }
 
     @Test
-    void getEngineAircrafts() {
+    void shouldGetAircraftByIdFromDataBaseAndSaveWhenUpdateAndIdEquals() {
+        //Given
+        Aircraft editedAircraft = AircraftTestBase.builder().build();
+        Aircraft toEditAircraft = AircraftTestBase.builder().build();
+        long id = toEditAircraft.getId();
+        Mockito.when(aircraftRepository.findFirstById(id)).thenReturn(toEditAircraft);
+        //When
+        testingObject.update(editedAircraft);
+        //then
+        verify(aircraftRepository, times(1)).save(toEditAircraft);
     }
 
     @Test
-    void getGliders() {
+    void shouldNotSaveWhenUpdateAndIdNotEquals() {
+        //Given
+        Aircraft editedAircraft = AircraftTestBase.builder().withId(2L).build();
+        Aircraft toEditAircraft = AircraftTestBase.builder().withId(3L).build();
+        long id = toEditAircraft.getId();
+        Mockito.when(aircraftRepository.findFirstById(id)).thenReturn(toEditAircraft);
+        //When
+        testingObject.update(editedAircraft);
+        //then
+        verify(aircraftRepository, never()).save(toEditAircraft);
+    }
+
+
+    @Test
+    void shouldChangeDataInAircraftToEditWhenUpdateAndIdEquals() {
+        //Given
+        Aircraft editedAircraft = AircraftTestBase.builder()
+                .withActive(false)
+                .withArc(LocalDate.now().plusDays(365))
+                .withNextWorkDate(LocalDate.now().plusDays(150))
+                .withFlyingTime(1000)
+                .withInsurance(LocalDate.now().plusDays(300))
+                .withNextWorkTime(9000)
+                .withNextWorkTimeDescription("50h")
+                .build();
+        Aircraft toEditAircraft = AircraftTestBase.builder().build();
+        long id = toEditAircraft.getId();
+        Mockito.when(aircraftRepository.findFirstById(id)).thenReturn(toEditAircraft);
+        //When
+        testingObject.update(editedAircraft);
+        //then
+        assertThat(editedAircraft.getActive()).isEqualTo(toEditAircraft.getActive());
+        assertThat(editedAircraft.getArc()).isEqualTo(toEditAircraft.getArc());
+        assertThat(editedAircraft.getNextWorkDate()).isEqualTo(toEditAircraft.getNextWorkDate());
+        assertThat(editedAircraft.getFlyingTime()).isEqualTo(toEditAircraft.getFlyingTime());
+        assertThat(editedAircraft.getInsurance()).isEqualTo(toEditAircraft.getInsurance());
+        assertThat(editedAircraft.getNextWorkTime()).isEqualTo(toEditAircraft.getNextWorkTime());
+        assertThat(editedAircraft.getNextWorkTimeDescription()).isEqualTo(toEditAircraft.getNextWorkTimeDescription());
+    }
+
+    @Test
+    void shouldGetSetOfEngineAircraftsUsingAircraftRepository() {
+        testingObject.getEngineAircrafts();
+
+        verify(aircraftRepository, times(1)).findByEngineTrueOrderByType();
+    }
+
+    @Test
+    void shouldGetSetOfGlidersUsingAircraftRepository() {
+        testingObject.getGliders();
+
+        verify(aircraftRepository, times(1)).findByEngineFalseOrderByType();
     }
 }
