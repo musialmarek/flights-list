d7c22374235b51cb8640019e0feffe5562f1262e
Marek Musiał <mmusialmarek@gmail.com>
2020-11-27 11:46:13 +0100

    changed Controller for showing list of glider-flights by Pilot, changed request parameters to FlightsFilter object as model attribute

--- src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightByPilotController.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/gliderFlight/GliderFlightByPilotController.java
@@ -6,10 +6,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.jgora.aeroklub.airflightslist.abstractFlight.FlightsFilter;
 import pl.jgora.aeroklub.airflightslist.aircraft.AircraftService;
-import pl.jgora.aeroklub.airflightslist.model.Aircraft;
 import pl.jgora.aeroklub.airflightslist.model.GliderFlight;
 import pl.jgora.aeroklub.airflightslist.model.Pilot;
 import pl.jgora.aeroklub.airflightslist.pilot.PilotService;
@@ -30,55 +31,37 @@ public class GliderFlightByPilotController {
     @GetMapping("/user/glider-flights")
     public String showUsersGliderFlights(@AuthenticationPrincipal CurrentUser user,
                                          Model model,
-                                         @RequestParam(name = "filter", required = false) Boolean filter,
-                                         @RequestParam(name = "from", required = false) String from,
-                                         @RequestParam(name = "to", required = false) String to,
-                                         @RequestParam(name = "task", required = false) String task,
-                                         @RequestParam(name = "pic", required = false) Boolean pic,
-                                         @RequestParam(name = "instructor", required = false) Boolean instructor,
-                                         @RequestParam(name = "aircraft", required = false) Long aircraft,
-                                         @RequestParam(name = "type", required = false) String type,
-                                         @RequestParam(name = "registration", required = false) String registration,
-                                         @RequestParam(name = "start", required = false) String start
-    ) {
-        Long id = user.getUser().getPilot().getId();
-        showGliderFlights(model, id, filter, true, from, to, task, pic, instructor, aircraft, type, registration, start);
+                                         @RequestParam(value = "filter", required = false) Boolean filter,
+                                         @ModelAttribute(name = "flightsFilter") FlightsFilter flightsFilter) {
+
+        Pilot pilot = user.getUser().getPilot();
+        flightsFilter.setPilot(pilot);
+        model.addAttribute("flightsFilter", new FlightsFilter());
+        showGliderFlights(model, filter, flightsFilter);
         return "flights/glider-by-user";
 
     }
 
     @GetMapping("admin/pilot/glider-flights")
-    public String showPilotsGliderFlights(
-            Model model,
-            @RequestParam("id") Long id,
-            @RequestParam(name = "filter", required = false) Boolean filter,
-            @RequestParam(name = "active", required = false) Boolean active,
-            @RequestParam(name = "from", required = false) String from,
-            @RequestParam(name = "to", required = false) String to,
-            @RequestParam(name = "task", required = false) String task,
-            @RequestParam(name = "pic", required = false) Boolean pic,
-            @RequestParam(name = "instructor", required = false) Boolean instructor,
-            @RequestParam(name = "aircraft", required = false) Long aircraft,
-            @RequestParam(name = "type", required = false) String type,
-            @RequestParam(name = "registration", required = false) String registration,
-            @RequestParam(name = "start", required = false) String start
-    ) {
-        showGliderFlights(model, id, filter, active, from, to, task, pic, instructor, aircraft, type, registration, start);
+    public String showPilotsGliderFlights(Model model,
+                                          @RequestParam(value = "id") Long id,
+                                          @RequestParam(value = "filter", required = false) Boolean filter,
+                                          @ModelAttribute(name = "flightsFilter") FlightsFilter flightsFilter) {
+        Pilot pilot = pilotService.findById(id);
+        flightsFilter.setPilot(pilot);
+        showGliderFlights(model, filter, flightsFilter);
         return "flights/glider-by-pilot";
     }
 
-    void showGliderFlights(Model model, @RequestParam("id") Long id, @RequestParam(name = "filter", required = false) Boolean filter, @RequestParam(name = "active", required = false) Boolean active, @RequestParam(name = "from", required = false) String from, @RequestParam(name = "to", required = false) String to, @RequestParam(name = "task", required = false) String task, @RequestParam(name = "pic", required = false) Boolean pic, @RequestParam(name = "instructor", required = false) Boolean instructor, @RequestParam(name = "aircraft", required = false) Long aircraft, @RequestParam(name = "type", required = false) String type, @RequestParam(name = "registration", required = false) String registration, @RequestParam(name = "start", required = false) String start) {
-        log.debug("\nGETTING PILOT WITH ID {} ", id);
-        Pilot pilot = pilotService.findById(id);
+    private void showGliderFlights(Model model, Boolean filter, FlightsFilter flightsFilter) {
+        log.debug("\nGETTING PILOT {} ", flightsFilter.getPilot());
+        Pilot pilot = flightsFilter.getPilot();
         StringBuilder sb = new StringBuilder();
         List<GliderFlight> flights = new ArrayList<>();
         if (pilot != null) {
-
             if (filter != null) {
                 log.debug("\n FILTER IS TRUE");
-                Aircraft gliderAircraft = aircraftService.findById(aircraft);
-                flights = gliderFlightService.getFilteredGliderFlightsByPilot(pilot, active, from, to, task, pic, instructor, gliderAircraft, type, registration, start);
-
+                flights = gliderFlightService.getFilteredGliderFlightsByPilot(flightsFilter);
             } else {
                 flights = gliderFlightService.getByPilot(pilot);
             }
@@ -92,5 +75,4 @@ public class GliderFlightByPilotController {
         model.addAttribute("pilot", pilot);
         model.addAttribute("aircrafts", aircraftService.getGliders());
     }
-
 }
