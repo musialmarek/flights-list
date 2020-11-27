a6c2dd3fb2e78aafb11b4bda6613423dde191855
Marek Musiał <mmusialmarek@gmail.com>
2020-11-27 11:50:31 +0100

    changed args in methods getting filtering flights from database and updated implementation

--- src/main/java/pl/jgora/aeroklub/airflightslist/abstractFlight/FilterFlightsRepositoryImpl.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/abstractFlight/FilterFlightsRepositoryImpl.java
@@ -9,7 +9,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.util.List;
-import java.util.Map;
 
 @Transactional
 @Slf4j
@@ -18,21 +17,21 @@ public class FilterFlightsRepositoryImpl implements FilterFlightsRepository {
     private EntityManager entityManager;
 
     @Override
-    public List<EngineFlight> getFilteredEngineFlights(String whereSection, Map<String, Object> filters) {
+    public List<EngineFlight> getFilteredEngineFlights(FlightsFilter flightsFilter) {
         log.debug("\nCREATING QUERY to FILTER ENGINE FLIGHTS BY PILOT");
-        TypedQuery<EngineFlight> query = entityManager.createQuery("SELECT f FROM EngineFlight f WHERE " + whereSection + "  ORDER BY f.date, f.start ", EngineFlight.class);
+        TypedQuery<EngineFlight> query = entityManager.createQuery("SELECT f FROM EngineFlight f WHERE " + flightsFilter.getWhereSection() + "  ORDER BY f.date, f.start ", EngineFlight.class);
         log.debug("\nSETTING PARAMETERS TO QUERY");
-        filters.forEach(query::setParameter);
+        flightsFilter.getFilters().forEach(query::setParameter);
         log.debug("\nQUERY {}", query.toString());
         return query.getResultList();
     }
 
     @Override
-    public List<GliderFlight> getFilteredGliderFlights(String whereSection, Map<String, Object> filters) {
+    public List<GliderFlight> getFilteredGliderFlights(FlightsFilter flightsFilter) {
         log.debug("\nCREATING QUERY to FILTER GLIDER FLIGHTS BY PILOT");
-        TypedQuery<GliderFlight> query = entityManager.createQuery("SELECT f FROM GliderFlight f WHERE " + whereSection + "  ORDER BY f.date, f.start ", GliderFlight.class);
+        TypedQuery<GliderFlight> query = entityManager.createQuery("SELECT f FROM GliderFlight f WHERE " + flightsFilter.getWhereSection() + "  ORDER BY f.date, f.start ", GliderFlight.class);
         log.debug("\nSETTING PARAMETERS TO QUERY");
-        filters.forEach(query::setParameter);
+        flightsFilter.getFilters().forEach(query::setParameter);
         log.debug("\nQUERY {}", query.toString());
         return query.getResultList();
     }
