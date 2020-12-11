81a79c0ae7f944e7e3bef39bcf40d8284db9222a
Marek Musiał <mmusialmarek@gmail.com>
2020-12-11 11:15:53 +0100

    created FlightBuilder for Flights Tests

---
+++ src/test/java/pl/jgora/aeroklub/airflightslist/abstractFlight/FlightTestBase.java
@@ -0,0 +1,92 @@
+package pl.jgora.aeroklub.airflightslist.abstractFlight;
+
+import pl.jgora.aeroklub.airflightslist.model.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class FlightTestBase {
+    static FlightBuilder builder(String type) {
+        return new FlightBuilder(type);
+    }
+
+    static class FlightBuilder {
+
+
+        AbstractFlight flight;
+
+        public FlightBuilder(String type) {
+            if (type.equals("glider")) {
+                this.flight = new GliderFlight();
+            } else if (type.equals("engine")) {
+                this.flight = new EngineFlight();
+            } else {
+                throw new IllegalArgumentException("unknown flight type " + type);
+            }
+        }
+
+        FlightBuilder withActive(Boolean active) {
+            flight.setActive(active);
+            return this;
+        }
+
+        FlightBuilder withRegistrationNumber(String registrationNumber) {
+            flight.setAircraftRegistrationNumber(registrationNumber);
+            return this;
+        }
+
+        FlightBuilder withPic(Pilot pic) {
+            flight.setPic(pic);
+            return this;
+        }
+
+        FlightBuilder withCopilot(Pilot copilot) {
+            flight.setCopilot(copilot);
+            return this;
+        }
+
+        FlightBuilder withPicName(String picName) {
+            flight.setPicName(picName);
+            return this;
+        }
+
+        FlightBuilder withCopilotName(String copilotName) {
+            flight.setCopilotName(copilotName);
+            return this;
+        }
+
+        FlightBuilder withDate(LocalDate date) {
+            flight.setDate(date);
+            return this;
+        }
+
+        FlightBuilder withStart(LocalTime startTime) {
+            flight.setStart(startTime);
+            return this;
+        }
+
+        FlightBuilder withTouchdown(LocalTime touchdownTime) {
+            flight.setTouchdown(touchdownTime);
+            return this;
+        }
+
+        FlightBuilder withTask(String task) {
+            flight.setTask(task);
+            return this;
+        }
+
+        FlightBuilder withInstructor(Boolean instructor) {
+            flight.setInstructor(instructor);
+            return this;
+        }
+
+        FlightBuilder withAircraft(Aircraft aircraft) {
+            flight.setAircraft(aircraft);
+            return this;
+        }
+
+        AbstractFlight build() {
+            return this.flight;
+        }
+    }
+}
