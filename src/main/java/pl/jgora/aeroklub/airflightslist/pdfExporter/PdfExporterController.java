de218f958533451460735910929665f966eb8a40
Marek Musiał <mmusialmarek@gmail.com>
2021-02-04 15:37:37 +0100

    deleted unnecessary lines

--- src/main/java/pl/jgora/aeroklub/airflightslist/pdfExporter/PdfExporterController.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/pdfExporter/PdfExporterController.java
@@ -13,17 +13,14 @@ import java.util.List;
 @Controller
 @Slf4j
 public class PdfExporterController {
-    //TODO extend for Glider-List and List by Pilot/User/Aircraft
     @PostMapping("/create-pdf")
     public void createPdf(@ModelAttribute(name = "pdf") PdfExporter pdf, HttpServletResponse response) throws IOException {
         response.setContentType("application/pdf");
         String headerKey = "Content-Disposition";
         String headerValue = "attachment; filename=list";
         log.debug("pdf is null {}", pdf == null);
-        assert pdf != null;
-        List<EngineFlight> flights = pdf.getEngineFlights();
-        log.debug("flights is null {}", flights == null);
         response.setHeader(headerKey, headerValue);
+        assert pdf != null;
         pdf.export(response);
     }
 }
