0fd9c5fbd1f1aa255f3d60aea386fef21bd9fda1
Marek Musiał <mmusialmarek@gmail.com>
2021-02-05 12:28:49 +0100

    change to creating new PdfExporter object in controller because of mechanisms in constructors

--- src/main/java/pl/jgora/aeroklub/airflightslist/pdfExporter/PdfExporterController.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/pdfExporter/PdfExporterController.java
@@ -4,23 +4,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import pl.jgora.aeroklub.airflightslist.model.EngineFlight;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @Controller
 @Slf4j
 public class PdfExporterController {
     @PostMapping("/create-pdf")
     public void createPdf(@ModelAttribute(name = "pdf") PdfExporter pdf, HttpServletResponse response) throws IOException {
+        log.debug("pdf is null {}", pdf == null);
+        assert pdf != null;
+        if (pdf.isGlider()) {
+            pdf = new PdfExporter(pdf.getType(), pdf.getGliderFlights());
+        } else {
+            pdf = new PdfExporter(pdf.getEngineFlights(), pdf.getType());
+        }
         response.setContentType("application/pdf");
         String headerKey = "Content-Disposition";
         String headerValue = "attachment; filename=list";
-        log.debug("pdf is null {}", pdf == null);
         response.setHeader(headerKey, headerValue);
-        assert pdf != null;
         pdf.export(response);
     }
 }
