00318d64de8fcc5412e0be1d57f7c3ba8bd41175
Marek Musiał <mmusialmarek@gmail.com>
2021-02-18 17:17:23 +0100

    added method to get all prices

--- src/main/java/pl/jgora/aeroklub/airflightslist/price/PriceService.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/price/PriceService.java
@@ -3,9 +3,16 @@ package pl.jgora.aeroklub.airflightslist.price;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pl.jgora.aeroklub.airflightslist.model.Price;
+
+import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class PriceService {
+    private final PriceRepository priceRepository;
+    public List<Price> findAll() {
+        return priceRepository.findAll();
+    }
 }
