0d94ef0ed2d0795306fd68cf45985ae0974ed31a
Marek Musiał <mmusialmarek@gmail.com>
2021-02-23 14:54:09 +0100

    created repository interface for entity "Account"

---
+++ src/main/java/pl/jgora/aeroklub/airflightslist/account/AccountRepository.java
@@ -0,0 +1,8 @@
+package pl.jgora.aeroklub.airflightslist.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.jgora.aeroklub.airflightslist.model.Account;
+
+public interface AccountRepository extends JpaRepository<Account,Long> {
+    Account findFirstByName(String name);
+}
