3f6b6134c44be1b433a7a7f2b8488139bd8c986f
Marek Musiał <mmusialmarek@gmail.com>
2021-02-23 14:30:29 +0100

    created new entity Account for accounts numbers

---
+++ src/main/java/pl/jgora/aeroklub/airflightslist/model/Account.java
@@ -0,0 +1,22 @@
+package pl.jgora.aeroklub.airflightslist.model;
+
+import lombok.*;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "accounts")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@ToString
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    Integer number;
+}
