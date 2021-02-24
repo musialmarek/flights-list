9b070c47cbdf9f4c8a81caabae5c7045039de173
Marek Musiał <mmusialmarek@gmail.com>
2021-02-24 16:38:34 +0100

    added method to get filtered notes

--- src/main/java/pl/jgora/aeroklub/airflightslist/abstractFlight/FilterFlightsRepository.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/abstractFlight/FilterFlightsRepository.java
@@ -2,6 +2,7 @@ package pl.jgora.aeroklub.airflightslist.abstractFlight;
 
 import pl.jgora.aeroklub.airflightslist.model.EngineFlight;
 import pl.jgora.aeroklub.airflightslist.model.GliderFlight;
+import pl.jgora.aeroklub.airflightslist.model.Note;
 
 import java.util.List;
 
@@ -9,5 +10,7 @@ public interface FilterFlightsRepository {
     List<EngineFlight> getFilteredEngineFlights(FlightsFilter flightsFilter);
 
     List<GliderFlight> getFilteredGliderFlights(FlightsFilter flightsFilter);
+
+    List<Note> getFilteredNotes(FlightsFilter filter);
 }
 
