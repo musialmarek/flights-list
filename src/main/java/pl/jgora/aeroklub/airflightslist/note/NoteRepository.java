292c371c57b721864bf53a034080d80d4d957db3
Marek Musiał <mmusialmarek@gmail.com>
2021-02-24 16:48:27 +0100

    added extends with FlightsFilterRepository

--- src/main/java/pl/jgora/aeroklub/airflightslist/note/NoteRepository.java
+++ src/main/java/pl/jgora/aeroklub/airflightslist/note/NoteRepository.java
@@ -1,11 +1,12 @@
 package pl.jgora.aeroklub.airflightslist.note;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.jgora.aeroklub.airflightslist.abstractFlight.FilterFlightsRepository;
 import pl.jgora.aeroklub.airflightslist.model.Note;
 
 import java.time.LocalDate;
 
-public interface NoteRepository extends JpaRepository<Note, Long> {
+public interface NoteRepository extends JpaRepository<Note, Long>, FilterFlightsRepository {
 
     int countByDateBetween(LocalDate yearBegin, LocalDate yearEnd);
 }
